9d414ccd7182fcd9a4aabff9947e5372093df191
Ian Brown <ibapps39@gmail.com>
2021-04-18 14:16:36 -0600

    Initial Commit

---
+++ iOSDevProject1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSDevProject1
+//
+//  Created by Ian Brown on 4/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSDevProject1/iOSDevProject1App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSDevProject1App.swift
+//  iOSDevProject1
+//
+//  Created by Ian Brown on 4/18/21.
+//
+
+import SwiftUI
+
+@main
+struct iOSDevProject1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
